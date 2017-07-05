87db92e90b0119438b0745cebe2dd70babe7987d
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-07-06 00:04:06 +0200

    Create match routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 'use strict'
 
 const express = require('express');
+const api = express.Router();
+
 const FieldCtrl = require('../controllers/field')
 const UserCtrl = require('../controllers/user');
+const MatchCtrl = require('../controllers/match');
+
 const { isAuth } = require('../middlewares/auth');
-const api = express.Router();
 
 // Fields Routes
 api.get('/field', FieldCtrl.getFields);
@@ -12,6 +15,13 @@ api.get('/field/:fieldId', FieldCtrl.getField);
 api.post('/field', FieldCtrl.saveField);
 api.put('/field/:fieldId', FieldCtrl.updateField);
 api.delete('/field/:fieldId', FieldCtrl.deleteField);
+
+// Match Routes
+api.get('/match', MatchCtrl.getMatchs);
+api.get('/match/:matchId', MatchCtrl.getMatch);
+api.post('/match', MatchCtrl.saveMatch);
+api.put('/match/:matchId', MatchCtrl.updateMatch);
+api.delete('/match/:matchId', MatchCtrl.deleteMatch);
 
 // Routes for manage users
 api.post('/signup', UserCtrl.signUp);
