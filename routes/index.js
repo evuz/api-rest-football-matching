a2770b896d73fc66203733feb84d464996ff1a81
a170828 <acalvoe@externos.bankia.com>
2017-07-07 12:36:20 +0200

    Create player routes

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const api = express.Router();
 const FieldCtrl = require('../controllers/field')
 const UserCtrl = require('../controllers/user');
 const MatchCtrl = require('../controllers/match');
+const PlayerCtrl = require('../controllers/player');
 
 const { isAuth } = require('../middlewares/auth');
 
@@ -22,6 +23,14 @@ api.get('/match/:matchId', MatchCtrl.getMatch);
 api.post('/match', MatchCtrl.saveMatch);
 api.put('/match/:matchId', MatchCtrl.updateMatch);
 api.delete('/match/:matchId', MatchCtrl.deleteMatch);
+
+// Player Routes
+api.get('/player', PlayerCtrl.getPlayers);
+api.get('/player/:playerId', PlayerCtrl.getPlayer);
+api.post('/player', isAuth, PlayerCtrl.savePlayer);
+api.post('/externalPlayer', PlayerCtrl.savePlayer);
+api.put('/player/:playerId', PlayerCtrl.updatePlayer);
+api.delete('/player/:playerId', PlayerCtrl.deletePlayer);
 
 // Routes for manage users
 api.post('/signup', UserCtrl.signUp);
