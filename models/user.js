4923e121813002415733360f076609d72d235451
a170828 <acalvoe@externos.bankia.com>
2017-07-07 12:38:15 +0200

    User: link with player profile

--- models/user.js
+++ models/user.js
@@ -6,39 +6,39 @@ const bcrypt = require('bcrypt-nodejs');
 const crypto = require('crypto');
 
 const UserSchema = new Schema({
-    playerId: String,
-    email: { type: String, unique: true, lowercase: true },
-    displayName: String,
-    avatar: String,
-    password: String,
-    signUpDate: { type: Date, default: Date.now() },
-    lastLogin: Date
+  playerId: { type: Schema.ObjectId, ref: 'Player' },
+  email: { type: String, unique: true, lowercase: true },
+  displayName: String,
+  avatar: String,
+  password: String,
+  signUpDate: { type: Date, default: Date.now() },
+  lastLogin: Date
 });
 
 UserSchema.pre('save', function (next) {
-    let user = this;
-    if (!user.isModified('password')) return next();
+  let user = this;
+  if (!user.isModified('password')) return next();
 
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) return next(err);
-        bcrypt.hash(user.password, salt, null, (err, hash) => {
-            if (err) next(err);
-            user.password = hash;
-            next();
-        })
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) next(err);
+      user.password = hash;
+      next();
     })
+  })
 });
 
-UserSchema.methods.validPassword = function(password) {
-    let user = this;
-    return bcrypt.compareSync(password, user.password);
+UserSchema.methods.validPassword = function (password) {
+  let user = this;
+  return bcrypt.compareSync(password, user.password);
 }
 
 UserSchema.methods.gravatar = function () {
-    if (!this.email) return 'https://gravatar.com/avatar/?s=200&d=retro';
+  if (!this.email) return 'https://gravatar.com/avatar/?s=200&d=retro';
 
-    const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-    return `https://gravatar.com/avatar/${md5}?s=200&d=retro`
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=200&d=retro`
 }
 
 module.exports = mongoose.model('User', UserSchema);
