a808b0ad36f5f8aa30180f121a18c76ed9a1cf67
evuz <jesus_jgb_45@hotmail.com>
2017-07-09 21:59:50 +0200

    Team Model: add tmp property

--- models/team.js
+++ models/team.js
@@ -7,6 +7,7 @@ const TeamSchema = new Schema({
   name: String,
   creationDate: { type: Date, default: Date.now() },
   stars: { type: Number, default: 0 },
+  tmp: { type: Boolean, default: false },
   players: [{ type: Schema.ObjectId, ref: 'Player' }]
 });
 
